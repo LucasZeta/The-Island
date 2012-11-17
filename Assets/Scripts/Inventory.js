6bc8845c889c7ba53e3e482bb7a03b6a8884f429
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-17 12:31:48 -0200

    Revert "Added power cell's charge GUI"
    This reverts commit 0969d7a0199ed2684fdbcd5b6b475a4830758a39.

--- Assets/Scripts/Inventory.js
+++ Assets/Scripts/Inventory.js
@@ -2,16 +2,20 @@
 
 static private var charge : int = 0;
 var collectSound : AudioClip;
-var hudCharge : Texture2D[];
-var chargeHudGUI : GUITexture;
 
 static function GetCharge() {
 	return charge;
 }
 
+function Start () {
+
+}
+
+function Update () {
+
+}
+
 function CellPickup() {
 	AudioSource.PlayClipAtPoint(collectSound, transform.position);
 	charge++;
-	
-	chargeHudGUI.texture = hudCharge[charge];
 }
