0969d7a0199ed2684fdbcd5b6b475a4830758a39
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-17 12:27:10 -0200

    Added power cell's charge GUI

--- Assets/Scripts/Inventory.js
+++ Assets/Scripts/Inventory.js
@@ -2,20 +2,16 @@
 
 static private var charge : int = 0;
 var collectSound : AudioClip;
+var hudCharge : Texture2D[];
+var chargeHudGUI : GUITexture;
 
 static function GetCharge() {
 	return charge;
 }
 
-function Start () {
-
-}
-
-function Update () {
-
-}
-
 function CellPickup() {
 	AudioSource.PlayClipAtPoint(collectSound, transform.position);
 	charge++;
+	
+	chargeHudGUI.texture = hudCharge[charge];
 }
