59fffc01dd69de1ce5f19aa1836d3a6e2c6d42ee
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-18 20:23:46 -0200

    Instantiating projectiles when pressing fire button

--- Assets/Scripts/CoconutThrower.js
+++ Assets/Scripts/CoconutThrower.js
@@ -1,9 +1,12 @@
 #pragma strict
 
 var throwSound : AudioClip;
+var projectile : Rigidbody;
 
 function Update () {
 	if(Input.GetButtonDown('Fire1')) {
 		audio.PlayOneShot(throwSound);
+		
+		var newProjectile : Rigidbody = Instantiate(projectile, transform.position, transform.rotation);
 	}
 }
