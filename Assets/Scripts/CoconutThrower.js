6a67eafd4ec4f5458fb5e7a1c839c1f4cb77e3be
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-18 20:33:35 -0200

    Added velocity to the newly instantiated projectiles

--- Assets/Scripts/CoconutThrower.js
+++ Assets/Scripts/CoconutThrower.js
@@ -1,12 +1,15 @@
 #pragma strict
 
-var throwSound : AudioClip;
 var projectile : Rigidbody;
+var throwSound : AudioClip;
+var throwSpeed : float = 30.0f;
 
 function Update () {
 	if(Input.GetButtonDown('Fire1')) {
 		audio.PlayOneShot(throwSound);
 		
 		var newProjectile : Rigidbody = Instantiate(projectile, transform.position, transform.rotation);
+		newProjectile.name = 'coconut';
+		newProjectile.velocity = transform.forward * throwSpeed;
 	}
 }
