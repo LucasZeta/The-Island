569ab8d0eb9b6893566fab989a2ec19a42f85bf1
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-18 15:00:15 -0200

    Added timer to the hint text disappear

--- Assets/Scripts/TextHints.js
+++ Assets/Scripts/TextHints.js
@@ -1,5 +1,19 @@
 #pragma strict
 
+var hintTime : float = 4.0f;
+private var timer : float = 0.0f;
+
+function Update() {
+	if(guiText.enabled) {
+		timer += Time.deltaTime;
+		
+		if(timer >= hintTime) {
+			guiText.enabled = false;
+			timer = 0.0f;
+		}
+	}
+}
+
 function ShowHint(message : String) {
 	guiText.text = message;
 	
