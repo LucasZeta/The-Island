fb6275a6260816d79dbb68e8296b56e0fbaddca8
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 14:21:13 -0200

    Added hint text when stepping into the Shack's mat

--- Assets/Scripts/ThrowTrigger.js
+++ Assets/Scripts/ThrowTrigger.js
@@ -1,9 +1,13 @@
 #pragma strict
 
 var crosshair : GUITexture;
+var textHints : GUIText;
 
 function OnTriggerEnter(collider : Collider) {
 	if(collider.gameObject.tag == 'Player') {
+		if(! CoconutWin.haveWon) {
+			textHints.SendMessage('ShowHint', "\n\n\n\n\n\nThere's a power cell attached to this game,\nmaybe I'll win it if I can knock down the targets...");
+		}
 		CoconutThrower.canThrow = true;
 		crosshair.enabled = true;
 	}
