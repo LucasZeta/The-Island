2029a59d1ded6c85cac9be21df449462fb9e22c1
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 14:33:57 -0200

    Handling the outpost door's animations using co-routine

--- Assets/Scripts/DoorManager.js
+++ Assets/Scripts/DoorManager.js
@@ -1,35 +1,25 @@
 #pragma strict
 
 private var doorIsOpen : boolean = false;
-private var doorTimer : float = 0.0f;
+
 var doorOpenTime : float = 3.0f;
 var doorOpenSound : AudioClip;
 var doorShutSound : AudioClip;
 
-function Start () {
-
-}
-
-function Update () {
-	if(doorIsOpen) {
-		doorTimer += Time.deltaTime;
-		
-		if(doorTimer >= doorOpenTime) {
-			MoveDoor(false, 'doorshut', doorShutSound);
-			doorTimer = 0;
-		}
-	}
-}
-
 function CheckDoor() {
 	if(! doorIsOpen) {
-		MoveDoor(true, 'dooropen', doorOpenSound);
+		MoveDoor();
 	}
 }
 
-function MoveDoor(open : boolean, animation : String, sound : AudioClip) {
-	doorIsOpen = open;
+function MoveDoor() {
+	doorIsOpen = true;
+	audio.PlayOneShot(doorOpenSound);
+	transform.parent.animation.Play('dooropen');
 	
-	audio.PlayOneShot(sound);
-	transform.parent.animation.Play(animation);
+	yield new WaitForSeconds(doorOpenTime);
+	
+	doorIsOpen = false;
+	audio.PlayOneShot(doorShutSound);
+	transform.parent.animation.Play('doorshut');
 }
