ea4c1c05ca213226227d5a9e7affda564c3f3df3
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 16:21:01 -0200

    Changed the hint text timer to use a co-routine
    Rather than incrementing a variable and checking if it is greater
    than an value in the Update function, changed to a co-routine,
    which costs less.

--- Assets/Scripts/TextHints.js
+++ Assets/Scripts/TextHints.js
@@ -1,23 +1,14 @@
 #pragma strict
 
 var hintTime : float = 4.0f;
-private var timer : float = 0.0f;
-
-function Update() {
-	if(guiText.enabled) {
-		timer += Time.deltaTime;
-		
-		if(timer >= hintTime) {
-			guiText.enabled = false;
-			timer = 0.0f;
-		}
-	}
-}
 
 function ShowHint(message : String) {
 	guiText.text = message;
 	
 	if(! guiText.enabled) {
 		guiText.enabled = true;
+		
+		yield new WaitForSeconds(hintTime);
+		guiText.enabled = false;
 	}
 }
