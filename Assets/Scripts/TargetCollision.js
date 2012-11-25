0cf9ddebc59a16061cd2bd19461f26975c2a9d4f
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-24 23:13:42 -0200

    Added co-routine to deal with the target animations

--- Assets/Scripts/TargetCollision.js
+++ Assets/Scripts/TargetCollision.js
@@ -13,6 +13,17 @@ function Start() {
 
 function OnCollisionEnter(collision : Collision) {
 	if((!beenHit) && collision.gameObject.name == 'coconut') {
-		
+		StartCoroutine('targetHit');
 	}
+}
+
+function targetHit() {
+	audio.PlayOneShot(hitSound);
+	targetRoot.Play('down');
+	beenHit = true;
+	
+	yield new WaitForSeconds(resetTime);
+	audio.PlayOneShot(resetSound);
+	targetRoot.Play('up');
+	beenHit = false;
 }
