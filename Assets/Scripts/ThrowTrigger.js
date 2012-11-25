92fe1e31ae8e1c384f98a3c1b58af2a877fdd115
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 12:44:15 -0200

    Enabled/dislabed crosshair via trigger collision

--- Assets/Scripts/ThrowTrigger.js
+++ Assets/Scripts/ThrowTrigger.js
@@ -1,13 +1,17 @@
 #pragma strict
 
+var crosshair : GUITexture;
+
 function OnTriggerEnter(collider : Collider) {
 	if(collider.gameObject.tag == 'Player') {
 		CoconutThrower.canThrow = true;
+		crosshair.enabled = true;
 	}
 }
 
 function OnTriggerExit(collider : Collider) {
 	if(collider.gameObject.tag == 'Player') {
 		CoconutThrower.canThrow = false;
+		crosshair.enabled = false;
 	}
 }
