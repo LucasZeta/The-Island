972c97b4707b1bb2c2f65dba8609c53e07495665
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 11:26:18 -0200

    Created script to verify if the Coconut Shy game has been won

---
+++ Assets/Scripts/CoconutWin.js
@@ -0,0 +1,24 @@
+#pragma strict
+
+static var targets : int = 0;
+static var haveWon : boolean = false;
+
+var winSound : AudioClip;
+var cellPrefab : GameObject;
+
+function Update() {
+	if((targets == 3) && (!haveWon)) {
+		targets = 0;
+		audio.PlayOneShot(winSound);
+		
+		var winCell : GameObject = transform.Find('powerCell').gameObject;
+		winCell.transform.Translate(-1, 0, 0);
+		
+		Instantiate(cellPrefab, winCell.transform.position, transform.rotation);
+		
+		Destroy(winCell);
+		haveWon = true;
+	}
+}
+
+@script RequireComponent(AudioSource)
