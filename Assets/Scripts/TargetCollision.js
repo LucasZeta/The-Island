57d07fda5524a39dbfccb930cf25a0a22988b5ff
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-25 11:40:05 -0200

    Incremented/decremented the target count
    Registering the number of targets down in order to the CoconutWin
    script be able to know when the mini-game has been won.

--- Assets/Scripts/TargetCollision.js
+++ Assets/Scripts/TargetCollision.js
@@ -21,11 +21,14 @@ function targetHit() {
 	audio.PlayOneShot(hitSound);
 	targetRoot.Play('down');
 	beenHit = true;
+	CoconutWin.targets++;
 	
 	yield new WaitForSeconds(resetTime);
+	
 	audio.PlayOneShot(resetSound);
 	targetRoot.Play('up');
 	beenHit = false;
+	CoconutWin.targets--;
 }
 
 @script RequireComponent(AudioSource)
