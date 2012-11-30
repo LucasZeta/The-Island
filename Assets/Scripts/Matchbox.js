303d0628505bd7bc32178fde5bcb9672988dca02
Lucas de Souza <lucasdsdc@hotmail.com>
2012-11-29 23:58:08 -0200

    Removed the handling of the MatchGUI of the Matchbox script
    It's more logical putting this knowledge in the Inventory script.

--- Assets/Scripts/Matchbox.js
+++ Assets/Scripts/Matchbox.js
@@ -1,12 +1,9 @@
 #pragma strict
 
-var matchGUI : GUITexture;
-
 function OnTriggerEnter(collider : Collider) {
 	if(collider.gameObject.tag == 'Player') {
 		collider.gameObject.SendMessage('MatchboxPickup');
 		
-		matchGUI.enabled = true;
 		Destroy(gameObject);
 	}
 }
