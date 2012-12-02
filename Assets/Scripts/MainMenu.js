68cba14804ab59debeddc52f51c3bba7d67b2221
Lucas de Souza <lucasdsdc@hotmail.com>
2012-12-02 00:35:18 -0200

    Added the instructions page

--- Assets/Scripts/MainMenu.js
+++ Assets/Scripts/MainMenu.js
@@ -1,12 +1,14 @@
 #pragma strict
 
 private var menuAreaNormalized : Rect;
+private var page : String;
 
 var menuSkin : GUISkin;
 var beepSound : AudioClip;
 var menuArea : Rect;
 var playButton : Rect;
 var instructionsButton : Rect;
+var instructions : Rect;
 var quitButton : Rect;
 
 function Start() {
@@ -16,6 +18,8 @@ function Start() {
 		menuArea.width,
 		menuArea.height
 	);
+	
+	page = 'main';
 }
 
 function OnGUI() {
@@ -23,31 +27,53 @@ function OnGUI() {
 	
 	GUI.BeginGroup(menuAreaNormalized);
 	
-	if(GUI.Button(Rect(playButton), 'Play')) {
-		ButtonAction('Island');
+	if(page == 'main') {
+		AddMainMenu();
 	}
-	
-	if(GUI.Button(Rect(instructionsButton), 'Instructions')) {
-		ButtonAction('Instructions');
-	}
-	
-	if(GUI.Button(Rect(quitButton), 'Quit')) {
-		ButtonAction('quit');
+	else {
+		AddInstructionsPage();
 	}
 	
 	GUI.EndGroup();
 }
 
-function ButtonAction(levelName : String) {
+function AddMainMenu() {
+	if(GUI.Button(Rect(playButton), 'Play')) {
+		ButtonAction('Island', 'level');
+	}
+	
+	if(GUI.Button(Rect(instructionsButton), 'Instructions')) {
+		ButtonAction('instructions', 'page');
+	}
+	
+	if(GUI.Button(Rect(quitButton), 'Quit')) {
+		ButtonAction('quit', 'level');
+	}
+}
+
+function AddInstructionsPage() {
+	GUI.Label(Rect(instructions), 'You awake on a mysterious island... Find a way to signal for help or face certain doom!');
+
+	if(GUI.Button(Rect(quitButton), 'Back')) {
+		ButtonAction('main', 'page');
+	}
+}
+
+function ButtonAction(name : String, type : String) {
 	audio.PlayOneShot(beepSound);
 	
 	yield new WaitForSeconds(0.35);
 	
-	if(levelName != 'quit') {
-		Application.LoadLevel(levelName);
+	if(type == 'level') {
+		if(name != 'quit') {
+			Application.LoadLevel(name);
+		}
+		else {
+			Application.Quit();
+		}
 	}
 	else {
-		Application.Quit();
+		page = name;
 	}
 }
 
