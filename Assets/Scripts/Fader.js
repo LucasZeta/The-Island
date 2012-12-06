e0c4e0599b4adfec1527517513963993f5e08503
Lucas Souza <lucasdsdc@hotmail.com>
2012-12-05 22:45:43 -0200

    Scaled the fader to any resolution

--- Assets/Scripts/Fader.js
+++ Assets/Scripts/Fader.js
@@ -2,6 +2,15 @@
 
 var loadingGUI : GUITexture;
 
+function Start() {
+	guiTexture.pixelInset = Rect(
+		-Screen.width * 0.5,
+		-Screen.height * 0.5,
+		Screen.width,
+		Screen.height
+	);
+}
+
 function LoadAnimation() {
 	Instantiate(loadingGUI);
 }
