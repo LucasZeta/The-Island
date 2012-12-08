99f25085ca9bb7dad2032720577113a6770c47f5
Lucas Souza <lucasdsdc@hotmail.com>
2012-12-08 21:46:58 -0200

    Removed behaviour that is now handled by other script
    Removed the function that auto-scaled the texture in the fader script
    because, now, there's another script to do that with any texture.

--- Assets/Scripts/Fader.js
+++ Assets/Scripts/Fader.js
@@ -2,15 +2,6 @@
 
 var loadingGUI : GUITexture;
 
-function Start() {
-	guiTexture.pixelInset = Rect(
-		-Screen.width * 0.5,
-		-Screen.height * 0.5,
-		Screen.width,
-		Screen.height
-	);
-}
-
 function LoadAnimation() {
 	Instantiate(loadingGUI);
 }
